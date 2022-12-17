6bc233ea01139e9a9dfb96f844ab3a1cbd35c68f
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-17 16:37:03 +0600

    Designing the Contact List

--- src/main/java/main/javafx/ContactsList/ContactsApp.java
+++ src/main/java/main/javafx/ContactsList/ContactsApp.java
@@ -6,10 +6,12 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -22,14 +24,75 @@ public class ContactsApp extends Application {
     public void start(Stage stage) {
         // Create a ListView to display the contacts
         ListView<String> listView = new ListView<>(contacts);
+        listView.setPrefSize(200, 250);
+        listView.setEditable(true);
 
         // Create a text field for the user to enter a new contact
         TextField textField = new TextField();
+        textField.setPromptText("Enter a new contact");
+        textField.setPrefColumnCount(20);
+        textField.setStyle(
+                        "-fx-font: 24 arial;" +
+                        "-fx-base: #b6e7c9;" +
+                        "-fx-text-fill: #0a0a0a;" +
+                        "-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);" +
+                        "-fx-background-radius: 5em; " +
+                        "-fx-background-insets: 0, 1, 2, 3, 0;"
+
+        );
 
         // Create buttons for adding, updating, and deleting contacts
         Button addButton = new Button("Add");
+        addButton.setStyle(
+                        "-fx-font: 24 arial;" +
+                        "-fx-base: #b6e7c9;" +
+                        "-fx-text-fill: #faf7f7;" +
+                        "-fx-background-radius: 2em; " +
+                        "-fx-background-insets: 0, 1, 2, 3, 0;" +
+                        "-fx-padding: 10 20 10 20;" +
+                        "-fx-border-radius: 5em; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-color: #fdf9f9;" +
+                        "-fx-border-insets: 0, 1, 2, 3, 0;" +
+                        "-fx-background-color: linear-gradient(#2acc6a, #67a474), " +
+                        "radial-gradient(center 50% -40%, radius 200%, #82b747 45%, #6eb93b 50%), " +
+                        "radial-gradient(center 50% -40%, radius 200%, #06e75d 45%, #379827 50%);"
+
+        );
         Button updateButton = new Button("Update");
+        updateButton.setStyle(
+                        "-fx-font: 24 arial;" +
+                        "-fx-base: #b6e7c9;" +
+                        "-fx-text-fill: #faf7f7;" +
+                        "-fx-background-radius: 2em; " +
+                        "-fx-background-insets: 0, 1, 2, 3, 0;" +
+                        "-fx-padding: 10 20 10 20;" +
+                        "-fx-border-radius: 5em; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-color: #fdf9f9;" +
+                        "-fx-border-insets: 0, 1, 2, 3, 0;" +
+                        "-fx-background-color: linear-gradient(#2acc6a, #67a474), " +
+                        "radial-gradient(center 50% -40%, radius 200%, #82b747 45%, #6eb93b 50%), " +
+                        "radial-gradient(center 50% -40%, radius 200%, #06e75d 45%, #379827 50%);"
+
+        );
         Button deleteButton = new Button("Delete");
+        deleteButton.setStyle(
+                        "-fx-font: 24 arial;" +
+                        "-fx-base: #b6e7c9;" +
+                        "-fx-text-fill: #faf7f7;" +
+                        "-fx-background-radius: 2em; " +
+                        "-fx-background-insets: 0, 1, 2, 3, 0;" +
+                        "-fx-padding: 10 20 10 20;" +
+                        "-fx-border-radius: 5em; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-color: #fdf9f9;" +
+                        "-fx-border-insets: 0, 1, 2, 3, 0;" +
+                        "-fx-background-color: linear-gradient(#2acc6a, #67a474), " +
+                        "radial-gradient(center 50% -40%, radius 200%, #82b747 45%, #6eb93b 50%), " +
+                        "radial-gradient(center 50% -40%, radius 200%, #06e75d 45%, #379827 50%);"
+
+        );
 
         // Handle the add button click
         addButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -87,9 +150,23 @@ public class ContactsApp extends Application {
 
         // Create a horizontal box for the buttons
         HBox buttonBox = new HBox(10, addButton, updateButton, deleteButton);
+        buttonBox.setStyle(
+                        "-fx-background-color: #b6e7c9;" +
+                        "-fx-padding: 10;" +
+                        "-fx-spacing: 10;" +
+                        "-fx-border-radius: 50em; " +
+                        "-fx-border-width: 2px; "
+        );
 
         // Create a vertical box for the ListView and text field
         VBox root = new VBox(10, listView, textField, buttonBox);
+        root.setStyle(
+                        "-fx-background-color: #b6e7c9;" +
+                        "-fx-padding: 10;" +
+                        "-fx-spacing: 10;" +
+                        "-fx-border-radius: 50em; " +
+                        "-fx-border-width: 2px; "
+        );
         root.setPadding(new Insets(10));
 
         // Create a scene and set it as the stage's scene
