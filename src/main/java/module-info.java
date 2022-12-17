88168f2f41fdb97e1c359a8652a107523774181c
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-17 16:58:25 +0600

    Revert "Designing the Contact List"
    This reverts commit 2276d19b38f88e94b02fbf80fa4a3d05b4a3df31.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module main.javafx {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens main.javafx to javafx.fxml;
     exports main.javafx;
@@ -14,16 +15,19 @@ module main.javafx {
     opens main.javafx.BMICalculator to javafx.fxml;
     exports main.javafx.TargetHeartRateCalculator;
     opens main.javafx.TargetHeartRateCalculator to javafx.fxml;
-    exports main.javafx.Painter;
-    opens main.javafx.Painter to javafx.fxml;
-    exports main.javafx.ContactsList;
-    opens main.javafx.ContactsList to javafx.fxml;
-    exports main.javafx.ContactListModification;
-    opens main.javafx.ContactListModification to javafx.fxml;
+//    exports main.javafx.PainterAppModification.Painter;
+    //opens main.javafx.PainterAppModification.Painter to javafx.fxml;
+    exports main.javafx.ContactsListAppModification;
+    opens main.javafx.ContactsListAppModification to javafx.fxml;
+    exports main.javafx.ContactListApp;
+    opens main.javafx.ContactListApp to javafx.fxml;
     exports main.javafx.TipCalculatorModification;
     opens main.javafx.TipCalculatorModification to javafx.fxml;
     exports main.javafx.ColorChooser;
     opens main.javafx.ColorChooser to javafx.fxml;
+    opens main.javafx.PainterAppModification to javafx.fxml;
+    exports main.javafx.PainterAppModification;
+    //exports main.javafx.PainterAppModification.Painter;
 //    exports main.javafx.WebBrowser;
 //    opens main.javafx.WebBrowser to javafx.fxml;
 }
