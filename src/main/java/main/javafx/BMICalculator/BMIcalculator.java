0474519e3e10f395417ef77604f9804c8c50f052
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-17 14:07:57 +0600

    designing the BMI Calculator

--- src/main/java/main/javafx/BMICalculator/BMIcalculator.java
+++ src/main/java/main/javafx/BMICalculator/BMIcalculator.java
@@ -6,132 +6,63 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-
 public class BMIcalculator extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("BMI Calculator");
-
         // Create a grid pane to hold the input fields and labels
         GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(8);
-        grid.setHgap(10);
-
-        // Add weight input field and label
-        Label weightLabel = new Label("Weight (kg): ");
-        GridPane.setConstraints(weightLabel, 0, 0);
-
-        TextField weightInput = new TextField();
-        weightInput.setStyle(
-                        "-fx-font-size: 14px;" +
-                        "-fx-font-weight: bold;" +
-                        "-fx-text-fill: #000000;" +
-                        "-fx-background-color: #ffffff;" +
-                        "-fx-border-color: #000000;" +
-                        "-fx-border-width: 1px;" +
-                        "-fx-border-radius: 5px;"
-
-        );
-        GridPane.setConstraints(weightInput, 1, 0);
-
-        // Add height input field and label
-        Label heightLabel = new Label("Height (cm): ");
-        heightLabel.setStyle(
-                    "-fx-font-size: 20px;" +
-                    "-fx-text-fill: #15151a;"
-        );
-        GridPane.setConstraints(heightLabel, 0, 1);
-        TextField heightInput = new TextField();
-        heightInput.setStyle(
-                        "-fx-font-size: 14px;" +
-                        "-fx-font-weight: bold;" +
-                        "-fx-text-fill: #000000;" +
-                        "-fx-background-color: #ffffff;" +
-                        "-fx-border-color: #000000;" +
-                        "-fx-border-width: 1px;" +
-                        "-fx-border-radius: 5px;"
-
-        );
-        GridPane.setConstraints(heightInput, 1, 1);
-
-        // Add BMI output label
-        Label bmiLabel = new Label("BMI: ");
-        GridPane.setConstraints(bmiLabel, 0, 2);
-        Label bmiOutput = new Label();
-        GridPane.setConstraints(bmiOutput, 1, 2);
-
-        // Add calculate button
-        Button calculateButton = new Button("Calculate");
-        GridPane.setConstraints(calculateButton, 1, 3);
-
-        // Add clear button
-        Button clearButton = new Button("Clear");
-        clearButton.setStyle(
-                "-fx-font-size: 18px;" +
-                "-fx-font-weight: bold;" +
-                "-fx-text-fill: #000000;" +
-                "-fx-background-color: #00ff00;" +
-                "-fx-border-color: #000000;" +
-                "-fx-border-width: 2px;" +
-                "-fx-border-radius: 5px;"
-        );;
-        GridPane.setConstraints(clearButton, 0, 4);
-
-        // Add exit button
-        Button exitButton = new Button("Exit");
-        GridPane.setConstraints(exitButton, 1, 4);
-
-
-        // Add all nodes to the grid pane
-        grid.getChildren().addAll(weightLabel, weightInput, heightLabel, heightInput, bmiLabel, bmiOutput, calculateButton, clearButton, exitButton);
-
-        // Calculate BMI when the calculate button is clicked
-        calculateButton.setOnAction(e -> {
-            double weight = Double.parseDouble(weightInput.getText());
-            double height = Double.parseDouble(heightInput.getText());
-            double bmi = weight / (height * height / 10000);
-            bmiOutput.setText(String.format("%.2f", bmi));
-        });
-
-        // Clear the input fields and output
-        clearButton.setOnAction(e -> {
-            weightInput.clear();
-            heightInput.clear();
-            bmiOutput.setText("");
-        });
-
-        // Exit the program
-
-        exitButton.setOnAction(e -> {
-            System.exit(0);
-        });
-
-
-        // Set the grid pane to the center of the scene
-
         grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
-        // Create a scene and place it in the stage
+        // Create the input fields and labels
+        TextField weightField = new TextField();
+        TextField heightField = new TextField();
+        Label bmiLabel = new Label();
 
-        Scene scene = new Scene(grid, 400, 300);
+        grid.add(new Label("Weight (kg):"), 0, 0);
+        grid.add(weightField, 1, 0);
+        grid.add(new Label("Height (cm):"), 0, 1);
+        grid.add(heightField, 1, 1);
+        grid.add(new Label("BMI:"), 0, 2);
+        grid.add(bmiLabel, 1, 2);
 
-        // Set the scene to the stage
+        // Create the calculate button and set its action
+        Button calcButton = new Button("Calculate BMI");
+        calcButton.setStyle("-fx-background-color: darkslateblue; " +
+                "-fx-text-fill: white;");
 
+        calcButton.setOnAction(event -> {
+            // Get the weight and height from the input fields
+            double weight = Double.parseDouble(weightField.getText());
+            double height = Double.parseDouble(heightField.getText());
+
+            // Calculate the BMI
+            double bmi = weight / (height / 100.0 * height / 100.0);
+
+            // Display the BMI in the label
+            bmiLabel.setText(String.format("%.1f", bmi));
+        });
+        grid.add(calcButton, 1, 3);
+
+        // Create the scene and set the CSS stylesheet
+        Scene scene = new Scene(grid, 350, 200);
+        scene.getStylesheets().add("java/main/javafx/BMICalculator/bmi-calculator.css");
+
+        // Set the scene and show the stage
         primaryStage.setScene(scene);
-
-        // Display the stage
-
+        primaryStage.setTitle("BMI Calculator");
         primaryStage.show();
     }
 }
-
-
